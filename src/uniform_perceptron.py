0d7a0b2ce4624efabdaebcf8850c9daa1f76122b
Franco Morroni <fmorroni99@gmail.com>
2025-05-10 19:13:18 -0300

    chore: rename

--- src/uniform_perceptron.py
+++ src/uniform_perceptron.py
@@ -12,11 +12,11 @@ class UniformPerceptron(SimplePerceptron):
             learn_rate: float = 0.1,
             max_epochs=1000,
             random_weight_initialize: bool = True,
-            func: PerceptronFunction = PerceptronFunction.HYPERBOLIC,
+            activation_func: PerceptronFunction = PerceptronFunction.HYPERBOLIC,
             beta: float = 0.1,
             min_error: float = 0.0001,
         ) -> None: 
-        self.func = func
+        self.activation_func = activation_func
         self.beta = beta
         self.min_error = min_error
         super().__init__(dataset, learn_rate, max_epochs, random_weight_initialize)
@@ -28,7 +28,7 @@ class UniformPerceptron(SimplePerceptron):
         return (delta**2) / 2
 
     def _activation_func(self, weighted_sum: np.float64) -> float:
-        return self.func.func(weighted_sum, self.beta)
+        return self.activation_func.func(weighted_sum, self.beta)
 
     def _calc_weight_adjustment(self, inputs: np.ndarray, delta: float) -> None:
-        self.weights += self.learn_rate * delta * inputs * self.func.deriv(np.dot(self.weights, inputs), self.beta)
+        self.weights += self.learn_rate * delta * inputs * self.activation_func.deriv(np.dot(self.weights, inputs), self.beta)
