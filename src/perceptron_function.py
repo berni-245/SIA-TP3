f835381a3413ec823fadbcdea779b5d1a2e71258
Franco Morroni <fmorroni99@gmail.com>
2025-05-12 00:05:34 -0300

    feat: data normalization for UniformPerceptron

--- src/perceptron_function.py
+++ src/perceptron_function.py
@@ -1,30 +1,35 @@
 from enum import Enum
 import numpy as np
 from numpy.typing import NDArray
-from typing import Any, Callable, Union
+from typing import Any, Callable, Tuple, Union
 
 
 class PerceptronFunction(Enum):
     HYPERBOLIC = (
         lambda x, beta: np.tanh(beta * x),
-        lambda x, beta: beta * (1 - np.tanh(beta * x) ** 2)
+        lambda x, beta: beta * (1 - np.tanh(beta * x) ** 2),
+        (-1, 1),
     )
     LOGISTICS = (
-    lambda x, beta: 1 / (1 + np.exp(-2 * beta * x)),
-    lambda x, beta: 2 * beta * (1 / (1 + np.exp(-2 * beta * x))) * (1 - (1 / (1 + np.exp(-2 * beta * x))))
+        lambda x, beta: 1 / (1 + np.exp(-2 * beta * x)),
+        lambda x, beta: 2 * beta * (1 / (1 + np.exp(-2 * beta * x))) * (1 - (1 / (1 + np.exp(-2 * beta * x)))),
+        (0, 1),
     )
     LINEAR = (
         lambda x, beta: x,
-        lambda x, beta: 1
+        lambda x, beta: 1,
+        None,
     )
 
     def __init__(
         self,
         func: Callable[[Union[float, NDArray[np.float64]], float], Union[float, NDArray[np.float64]]],
-        deriv: Callable[[Union[float, NDArray[np.float64]], float], Union[float, NDArray[np.float64]]]
+        deriv: Callable[[Union[float, NDArray[np.float64]], float], Union[float, NDArray[np.float64]]],
+        image: Tuple[float, float] | None,
     ):
         self._func = func
         self._deriv = deriv
+        self.image = image
 
     def func(self, x: Union[float, NDArray[np.float64]], beta: float) -> Any:
         return self._func(x, beta)
--- src/uniform_perceptron.py
+++ src/uniform_perceptron.py
@@ -12,14 +12,19 @@ class UniformPerceptron(SimplePerceptron):
             max_epochs=1000,
             random_weight_initialize: bool = True,
             activation_func: PerceptronFunction = PerceptronFunction.HYPERBOLIC,
-            beta: float = 0.1,
-            min_error: float = 0.0001,
+            beta: float = 1,
+            min_error: float = 1,
             copy_dataset = False,
         ) -> None: 
+        super().__init__(dataset, learn_rate, max_epochs, random_weight_initialize, copy_dataset, min_error)
         self.activation_func = activation_func
         self.beta = beta
-        self.min_error = min_error
-        super().__init__(dataset, learn_rate, max_epochs, random_weight_initialize, copy_dataset)
+        if activation_func.image != None:
+            Y = dataset['ev']
+            min_ev = np.min(Y)
+            max_ev = np.max(Y)
+            (min, max) = activation_func.image
+            dataset['ev'] = min + ((Y - min_ev)*(max - min))/(max_ev - min_ev)
 
     def has_next(self):
         return np.abs(self.error) > self.min_error and self.current_epoch < self.max_epochs
@@ -32,3 +37,5 @@ class UniformPerceptron(SimplePerceptron):
 
     def _calc_weight_adjustment(self, inputs: np.ndarray, delta: float) -> None:
         self.weights += self.learn_rate * delta * inputs * self.activation_func.deriv(np.dot(self.weights, inputs), self.beta)
+
+
