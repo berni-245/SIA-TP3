fix(maybe?): unnormalize values for error calculation

src/uniform_perceptron.py
@@ -24,18 +24,20 @@ class UniformPerceptron(SimplePerceptron):
         self.accumulated_weights = np.zeros(len(self.col_labels))
         self.min_data_batch_for_update = max(1, min(batch_update, len(self.dataset.index))) 
         self.data_update_count = 0
+        self.normalization_constant = 1
         if activation_func.image != None:
             Y = self.dataset['ev']
             self.min_ev = np.min(Y)
             self.max_ev = np.max(Y)
-            (min_val, max_val) = activation_func.image
-            self.dataset['ev'] = min_val + ((Y - self.min_ev)*(max_val - min_val))/(self.max_ev - self.min_ev)
+            (min_image, max_image) = activation_func.image
+            self.normalization_constant = (max_image - min_image)/(self.max_ev - self.min_ev)
+            self.dataset['ev'] = min_image + (Y - self.min_ev)*self.normalization_constant
 
     def has_next(self):
         return np.abs(self.error) > self.min_error and self.current_epoch < self.max_epochs
     
     def _calc_error_per_data(self, delta: float):
-        return (delta**2) / 2
+        return ((delta/self.normalization_constant)**2) / 2
 
     def _activation_func(self, weighted_sum: np.float64) -> float:
         return self.activation_func.func(weighted_sum, self.beta)
@@ -52,5 +54,5 @@ class UniformPerceptron(SimplePerceptron):
         output = super().try_current_epoch(inputs)
         if self.activation_func.image != None:
             (min, max) = self.activation_func.image
-            output = (output - min)*(self.max_ev - self.min_ev)/(max - min) + self.min_ev
+            output = (output - min)/self.normalization_constant + self.min_ev
         return output